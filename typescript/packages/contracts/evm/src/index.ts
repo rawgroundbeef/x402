fe9a5200bf162aa803bd28d4802ec32b21ba7f22
CarsonRoscoe <carsonroscoe7@gmail.com>
2026-01-06 17:59:13 -0800

    feat: added and implemented @x402/evm-contracts

---
+++ typescript/packages/contracts/evm/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @x402/contracts - Smart contracts for x402 payment protocol
+ */
+
+// Constants
+export { PERMIT2_ADDRESS, X402_PERMIT2_PROXY_ADDRESS } from "./constants";
+
+// ABIs (auto-generated)
+export * from "./abis";
---
+++ typescript/packages/contracts/evm/test/hardhat-types.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Type declarations to help TypeScript LSP understand Hardhat's ethers extensions
+ * This file is only for IDE type checking - the actual types work fine at runtime
+ */
+
+import "hardhat/types/runtime";
+import "@nomicfoundation/hardhat-toolbox";
